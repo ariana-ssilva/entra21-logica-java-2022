c393a98ffa1bc5344d68fc90980ad1afea4df592
ariana-ssilva <arianamoreira5021@gmail.com>
2022-05-01 17:54:43 -0300

    Atualizando arquivos README.md.

---
+++  Aula03 -  ExercicioProdutoComAcrescimo/README.md
@@ -0,0 +1,13 @@
+# Exercise 3.7 - Calculating the sale price
+
+Make a program that requests the name and cost price of any product. Afterwards, process a 65% increase and inform what the sale price will be.
+Increasing a percentage value is obtained mathematically by adding the value of the product to the result of the calculation: product * (65/100), that is,
+increase = product + product * (60/100).
+
+<center>
+
+![Gif AreaAndPerimeter](/gif_img/3.7.gif)
+
+</center>
+
+[Run Code](https://replit.com/@ariana-ssilva/Main-5#Main.java)
---  Aula03 -  ExercicioProdutoComAcrescimo/src/br/com/entra21/exercicio2/Main.java
+++  Aula03 -  ExercicioProdutoComAcrescimo/src/br/com/entra21/exercicio2/Main.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		float precoOriginal, precoComAumento;
-		String nomeProduto;
-		
-		
-		Scanner entrada = new Scanner(System.in);
-		
-		System.out.println("Digite o nome do produto:");
-		nomeProduto = entrada.next();
-		
-		System.out.println("Digite o preço do produto:");
-		precoOriginal = entrada.nextFloat();
-		
-		precoComAumento = precoOriginal + (precoOriginal*65/100);
-		
-		System.out.println("O preço do produto "+ nomeProduto+ 
-				" com aumento de 65& é de :" + precoComAumento);
-		
-		
+		 /*
+		 * Make a program that requests the name and cost price of any product.
+		 * Afterwards, process a 65% increase and inform what the sale price will be.
+		 * Increasing a percentage value is obtained mathematically by adding the value
+		 * of the product to the result of the calculation: product * (65/100), that is,
+		 * increase = product + product * (60/100).
+		 */
+		float costPrice, salePrice;
+		String productsName;
+
+		Scanner enter = new Scanner(System.in);
+
+		System.out.println("Enter the product's name:");
+		productsName = enter.next();
+
+		System.out.println("Enter the cost price:");
+		costPrice = enter.nextFloat();
+
+		salePrice = costPrice + (costPrice * 65 / 100);
+
+		System.out.println("The " + productsName +"'s sale price is: " + salePrice);
+
 	}
 
 }
